4e566a9d1828c92c15167de4062651ecd635805c
Mohamad Nassar <mnassar57@gmail.com>
2022-09-30 20:12:15 +0300

    firstAPI second sorting implemented

--- example-app/app/Http/Controllers/APIController.php
+++ example-app/app/Http/Controllers/APIController.php
@@ -12,7 +12,17 @@ class APIController extends Controller
         //Sort array alphabatically (numbers are at the start now)
         usort($arr, 'strcasecmp');
 
+        //Use usort to sort numbers after characters
+        usort($arr , function ($a, $b){
+            if(is_numeric($a) && !is_numeric($b))
+                return 1;
+            else if(!is_numeric($a) && is_numeric($b))
+                return -1;
+        });
+
         print_r($arr);
+
+        
     }
 
 
